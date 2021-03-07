4501d8771bd39795914c49710b11fd1d7fc17050
unknown <abner.f.c.r@hotmail.com>
2021-03-07 03:25:51 -0300

    adding floating action cart button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Home.css'
 import data from '../../data/products.json'
+import Fab from '@material-ui/core/Fab';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import linkedinIcon from '../../assets/linkedinIcon.png'
 import githubIcon from '../../assets/githubIcon.png'
 import instagramIcon from '../../assets/instagramIcon.png'
@@ -27,6 +29,9 @@ class Home extends React.Component {
             <>
                 <div className="home">
                     <div className="content">
+                        <Fab color="primary" aria-label="add" className="test">
+                            <ShoppingCartIcon fontSize="large" />
+                        </Fab>
                         <div className="carousel-home">
                             <div className="carousel-text">
                                 Frete Grátis <br />
@@ -40,26 +45,26 @@ class Home extends React.Component {
                             <div className="products-list">
                                 {this.state.products.map((product) => (
                                     <Card className="product-card">
-                                        <Card.Img variant="top" src={`./${product.image}` }/>
+                                        <Card.Img variant="top" src={`./${product.image}`} />
                                         <Card.Body>
                                             <Card.Title>{product.name}</Card.Title>
                                             <Card.Text>
-                                                Preço: {product.price}<br />
+                                                Preço: R$ {product.price}<br />
                                                 Score: {product.score}
                                             </Card.Text>
                                             <Button variant="primary">Adicionar no carrinho</Button>
                                         </Card.Body>
                                     </Card>
                                 ))}
-                                
+
                             </div>
                         </section>
 
                         <div className="footer">
                             <div className="social-medias">
-                                <a className="media-buttons" href="https://www.linkedin.com/in/abner-filipe/" target = "_blank"><img src={linkedinIcon}/></a>
-                                <a className="media-buttons" href="https://www.instagram.com/abnerfilipe/" target = "_blank"><img src={instagramIcon} /></a>
-                                <a className="media-buttons" href="https://github.com/abner423" target = "_blank"><img src={githubIcon} /></a>
+                                <a className="media-buttons" href="https://www.linkedin.com/in/abner-filipe/" target="_blank"><img src={linkedinIcon} /></a>
+                                <a className="media-buttons" href="https://www.instagram.com/abnerfilipe/" target="_blank"><img src={instagramIcon} /></a>
+                                <a className="media-buttons" href="https://github.com/abner423" target="_blank"><img src={githubIcon} /></a>
                             </div>
                         </div>
 
