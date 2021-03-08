9d39e6c72bb81305d97c4d54a45cf5218501d1d3
unknown <abner.f.c.r@hotmail.com>
2021-03-08 01:28:41 -0300

    adding label for sorting filter

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -133,94 +133,100 @@ class Home extends React.Component {
                         <section className="products-container">
                             <div className="products-container-title">
                                 <h2>Games</h2>
-                                <Select
-                                    defaultValue=' '
-                                    labelId="demo-controlled-open-select-label"
-                                    id="demo-controlled-open-select"
-                                    value={this.state.filter}
-                                    onChange={(evento) => {
-                                    this.setState({filter: evento.target.value,
-                                        products: this.state.products.sort(function (a,b){
-                                            if(evento.target.value == "name"){
-                                                if (a.name > b.name) {
-                                                    return 1;
-                                                  }
-                                                  if (a.name < b.name) {
-                                                    return -1;
-                                                  }
-                                                  // a must be equal to b
-                                                  return 0;
-                                            }else if(evento.target.value == "price"){
-                                                if (a.price > b.price) {
-                                                    return 1;
-                                                  }
-                                                  if (a.price < b.price) {
-                                                    return -1;
-                                                  }
-                                                  // a must be equal to b
-                                                  return 0;
-                                            }else if(evento.target.value == "score"){
-                                                if (a.score > b.score) {
-                                                    return 1;
-                                                  }
-                                                  if (a.score < b.score) {
-                                                    return -1;
-                                                  }
-                                                  // a must be equal to b
-                                                  return 0;
-                                            }
-                                        })})
-                                    console.log("trocou o state ",this.state)
-                                }}
-                                >
-                                    <MenuItem value={"price"}>Preço</MenuItem>
-                                    <MenuItem value={"score"}>Popularidade</MenuItem>
-                                    <MenuItem value={"name"}>Ordem Alfabética</MenuItem>
-                                </Select>
-                            </div>
-                                <div className="products-list">
-                                    {this.state.products.map((product) => (
-                                        <Card className="product-card">
-                                            <Card.Img variant="top" src={`./${product.image}`} />
-                                            <Card.Body>
-                                                <Card.Title>{product.name}</Card.Title>
-                                                <Card.Text>
-                                                    Preço: R$ {product.price}<br />
-                                                Score: {product.score}
-                                                </Card.Text>
-                                                <Button variant="primary" onClick={() => {
-                                                    this.props.addCart(product)
-                                                    this.props.calculateShipping()
-                                                    console.log("teste", this.props)
-                                                }}>Adicionar no carrinho</Button>
-                                            </Card.Body>
-                                        </Card>
-                                    ))}
+                                <div>
 
+                                    <InputLabel id="demo-simple-select-label">Ordenar por</InputLabel>
+                                    <Select
+                                        defaultValue=' '
+                                        labelId="demo-controlled-open-select-label"
+                                        id="demo-controlled-open-select"
+                                        value={this.state.filter}
+                                        onChange={(evento) => {
+                                            this.setState({
+                                                filter: evento.target.value,
+                                                products: this.state.products.sort(function (a, b) {
+                                                    if (evento.target.value == "name") {
+                                                        if (a.name > b.name) {
+                                                            return 1;
+                                                        }
+                                                        if (a.name < b.name) {
+                                                            return -1;
+                                                        }
+                                                        // a must be equal to b
+                                                        return 0;
+                                                    } else if (evento.target.value == "price") {
+                                                        if (a.price > b.price) {
+                                                            return 1;
+                                                        }
+                                                        if (a.price < b.price) {
+                                                            return -1;
+                                                        }
+                                                        // a must be equal to b
+                                                        return 0;
+                                                    } else if (evento.target.value == "score") {
+                                                        if (a.score > b.score) {
+                                                            return 1;
+                                                        }
+                                                        if (a.score < b.score) {
+                                                            return -1;
+                                                        }
+                                                        // a must be equal to b
+                                                        return 0;
+                                                    }
+                                                })
+                                            })
+                                            console.log("trocou o state ", this.state)
+                                        }}
+                                    >
+                                        <MenuItem value={"price"}>Preço</MenuItem>
+                                        <MenuItem value={"score"}>Popularidade</MenuItem>
+                                        <MenuItem value={"name"}>Ordem Alfabética</MenuItem>
+                                    </Select>
                                 </div>
+                            </div>
+                            <div className="products-list">
+                                {this.state.products.map((product) => (
+                                    <Card key={product.id} className="product-card">
+                                        <Card.Img variant="top" src={`./${product.image}`} />
+                                        <Card.Body>
+                                            <Card.Title>{product.name}</Card.Title>
+                                            <Card.Text>
+                                                Preço: R$ {product.price}<br />
+                                                    Score: {product.score}
+                                            </Card.Text>
+                                            <Button variant="primary" onClick={() => {
+                                                this.props.addCart(product)
+                                                this.props.calculateShipping()
+                                                console.log("teste", this.props)
+                                            }}>Adicionar no carrinho</Button>
+                                        </Card.Body>
+                                    </Card>
+                                ))}
+
+                            </div>
                         </section>
 
-                            <div className="footer">
-                                <div className="social-medias">
-                                    <a className="media-buttons" href="https://www.linkedin.com/in/abner-filipe/" target="_blank"><img src={linkedinIcon} /></a>
-                                    <a className="media-buttons" href="https://www.instagram.com/abnerfilipe/" target="_blank"><img src={instagramIcon} /></a>
-                                    <a className="media-buttons" href="https://github.com/abner423" target="_blank"><img src={githubIcon} /></a>
-                                </div>
+                        <div className="footer">
+                            <div className="social-medias">
+                                <a className="media-buttons" href="https://www.linkedin.com/in/abner-filipe/" target="_blank"><img src={linkedinIcon} /></a>
+                                <a className="media-buttons" href="https://www.instagram.com/abnerfilipe/" target="_blank"><img src={instagramIcon} /></a>
+                                <a className="media-buttons" href="https://github.com/abner423" target="_blank"><img src={githubIcon} /></a>
                             </div>
+                        </div>
 
                     </div>
 
-                    </div>
+                </div>
             </>
         );
     }
 } const mapStateToProps = state => ({
-                    ...state.Home
-                });
+    ...state.Home
+});
 
 function mapDispatchToProps(dispatch) {
     return {
-                    addCart: value => dispatch(addCart(value)),
+        addCart: value => dispatch(addCart(value)),
         alterQuantity: (product, action) => dispatch(alterQuantity(product, action)),
         calculatePrice: () => dispatch(calculatePrice()),
         clearCart: () => dispatch(clearCart()),
